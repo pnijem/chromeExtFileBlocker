a11db1417c11de954f2615c30fb4d001d6c9a383
pnijem <42067687+pnijem@users.noreply.github.com>
2019-02-14 11:44:19 +0200

    chrome extension files

---
+++ chromeExtension/bgEvent.js
@@ -0,0 +1,83 @@
+ var factory = new Factory();
+
+function Factory() { //Factory of FileExtension Objects
+    this.createExtension = function (ext) {
+        var extension;
+
+        if (ext === ".exe") {
+            extension = new Executable();
+        } else if (type === ".zip") {
+            extension = new Archived();
+        }/* else if (type == "someExt"){
+            extension = new SomeFutureExt();
+        }*/
+
+
+        extension.blockFileIfNeeded = function (item) {
+          let bool;
+          $.ajax({
+            type: "POST",
+            headers: {
+                  'Access-Control-Allow-Origin' : '*',
+                  'Access-Control-Allow-Credentials' : true
+            },
+            url: "https://l0ypwxusle.execute-api.ap-south-1.amazonaws.com/test" + extension.resource,
+            data: JSON.stringify(item.url),
+            dataType: "text",
+            success: function( data, textStatus, jQxhr ){
+              if(data=='true'){
+                console.log("The file download is blocked. Canceling the file download");
+                chrome.downloads.cancel(item.id);
+                bool = true;
+              }else{
+                chrome.downloads.resume(item.id);
+              }
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log("An error has occurred. Details: responseText - "+
+                jqXhr.responseText + ", statusText - "+jqXhr.statusText);
+            }
+          });
+          return bool;
+        }
+
+        return extension;
+    }
+}
+
+//List of File Type Objects
+var Executable = function () {
+    this.resource = "/isexecutable";
+};
+
+var Archived = function () {
+    this.resource = "/isarchived";
+};
+
+//End of List
+chrome.downloads.onDeterminingFilename.addListener(function(item) {
+try{
+  chrome.downloads.pause(item.id);
+  let rules = localStorage.rules;//see options.html
+  try {
+    rules = JSON.parse(rules);
+  } catch (e) {
+    localStorage.rules = JSON.stringify([]);
+  }
+
+  for (var index = 0; index < rules.length; ++index) {
+    let isBlocked;
+    var rule = rules[index];
+     if (rule.enabled) {
+       var ext = rule.ext_param;
+       isBlocked = factory.createExtension(ext).blockFileIfNeeded(item);
+       if(isBlocked){
+         break;
+       }
+     }
+  }
+}catch(e){
+  console.log("an error has occurred. Canceling the file download");
+  chrome.downloads.cancel(item.id);
+}
+});
---
+++ chromeExtension/options.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+  loadRules();
+  document.getElementById('new').onclick = function() {
+    new Rule();
+  };
+}
+
+Rule.prototype.getElement = function(name) {
+  return document.querySelector('#' + this.node.id + ' .' + name);
+}
+
+Rule.next_id = 0;
+
+function loadRules() {
+  var rules = localStorage.rules;
+  try {
+    JSON.parse(rules).forEach(function(rule) {
+      new Rule(rule);
+    });
+  } catch (e) {
+    console.log("Loading Rules has Failed");
+    localStorage.rules = JSON.stringify([]);
+  }
+}
+
+
+function Rule(data) {
+  var rules = document.getElementById('rules');
+  this.node = document.getElementById('rule-template').cloneNode(true);
+  this.node.id = 'rule' + (Rule.next_id++);
+  this.node.rule = this;
+  rules.appendChild(this.node);
+  this.node.hidden = false;
+
+  if (data) {
+    this.getElement('fileTypeSlector').value = data.fileTypeSlector;
+    this.getElement('ext-param').value = data.ext_param;
+    this.getElement('action').value = data.action;
+    this.getElement('enabled').checked = data.enabled;
+  }
+
+  this.getElement('enabled-label').htmlFor = this.getElement('enabled').id =
+    this.node.id + '-enabled';
+
+
+  this.getElement('fileTypeSlector').onchange = storeRules;
+  this.getElement('ext-param').onkeyup = storeRules;
+  this.getElement('action').onchange = storeRules;
+  this.getElement('enabled').onchange = storeRules;
+
+  var rule = this;
+
+  this.getElement('remove').onclick = function() {
+    rule.node.parentNode.removeChild(rule.node);
+    storeRules();
+  };
+
+  storeRules();
+}
+
+
+function storeRules() {
+  localStorage.rules = JSON.stringify(Array.prototype.slice.apply(
+    document.getElementById('rules').childNodes).map(function(node) {
+
+    return {fileTypeSlector: node.rule.getElement('fileTypeSlector').value,
+            ext_param: node.rule.getElement('ext-param').value,
+            action: node.rule.getElement('action').value,
+            enabled: node.rule.getElement('enabled').checked};
+  }));
+}
